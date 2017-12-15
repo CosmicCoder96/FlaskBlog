27e86d2365ef50dd700776f8790e7cf57cc5f62a
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-12-16 02:34:24 +0530

    Add shell context

--- blog.py
+++ blog.py
@@ -1,3 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post' :Post}
